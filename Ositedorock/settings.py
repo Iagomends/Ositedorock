c7cea5c58c0afd5dfb363a10de9d40f0da2460a6
Iago <iago_mendes@usp.br>
2023-11-19 13:01:55 -0300

    comm it

--- Ositedorock/settings.py
+++ Ositedorock/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_heroku # adicione esta linha
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +86,11 @@ DATABASES = {
     }
 }
 
-
+if os.environ.get("DATABASE_URL"):
+    DATABASES["default"] = dj_database_url.config(
+        conn_max_age=600,
+    )
+    
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
