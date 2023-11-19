fdda7163bcdb7c448b8d1ce2c62e750c4954503e
Iago <iago_mendes@usp.br>
2023-11-19 14:30:19 -0300

    commi t

--- build.sh
+++ build.sh
@@ -16,4 +16,5 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
-    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])EOF
+    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+EOF
